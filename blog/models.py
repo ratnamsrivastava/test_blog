f947cc28aaff4d1e33ee786faedc3307de311a86
Ratnam <ratnamsrivastava31@gmail.com>
2016-11-26 15:26:46 +0530

    first commit incoming

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
