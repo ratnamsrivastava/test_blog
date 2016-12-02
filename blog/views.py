a85e035bd69e1c47949307c904feb7c198e3918f
Ratnam <ratnamsrivastava31@gmail.com>
2016-12-02 16:39:07 +0530

    'Form_created'

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import Post_Form
 
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
-    return render(request,'blog/post_list.html',{'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = Post_Form()
+    return render(request, 'blog/post_new.html', {'form' : form})
