542df06455fca7fd9d1f305e4061e1f1ad669f82
Ratnam <ratnamsrivastava31@gmail.com>
2016-12-02 17:41:40 +0530

    'Finished'

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import Post_Form
@@ -16,5 +16,31 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = Post_Form()
-    return render(request, 'blog/post_new.html', {'form' : form})
+    if request.method == 'POST':
+        form = Post_Form(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+            # endif
+    # endif
+    else:
+        form = Post_Form()
+    return render(request, 'blog/post_new.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = Post_Form(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Post_Form(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
